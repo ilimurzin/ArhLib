a573c0753a7856dbc71f6b526f747ea4edd90534
ilimurzin <ilimurzin@ya.ru>
2018-08-13 14:47:44 +0300

    replaced oss-licenses plugin with own implementation

--- app/src/main/java/ru/arhlib/app/services/LicensesActivity.java
+++ app/src/main/java/ru/arhlib/app/services/LicensesActivity.java
@@ -1,32 +1,29 @@
 package ru.arhlib.app.services;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
+import android.webkit.WebView;
 
-import ru.arhlib.app.BuildConfig;
+import ru.arhlib.app.MyWebViewClient;
 import ru.arhlib.app.R;
 
-public class AboutActivity extends AppCompatActivity {
+public class LicensesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about_activity);
+        setContentView(R.layout.contacts_activity);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        String version = getString(R.string.about_version) + " " + BuildConfig.VERSION_NAME;
-        TextView v = findViewById(R.id.version);
-        v.setText(version);
+        WebView webView = findViewById(R.id.contacts_webview);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("file:///android_res/raw/licenses.html");
     }
 
     @Override
@@ -38,13 +35,5 @@ public class AboutActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    public void contactDeveloper(View v) {
-        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.about_author_email))));
-    }
-
-    public void showLicences(View v) {
-        startActivity(new Intent(this, LicensesActivity.class));
     }
 }
