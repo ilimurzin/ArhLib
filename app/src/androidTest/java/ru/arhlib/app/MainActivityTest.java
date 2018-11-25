291582ffc563e41454af0059dfb9a1680da39607
ilimurzin <ilimurzin@ya.ru>
2018-11-25 14:54:00 +0300

    updated test

--- app/src/androidTest/java/ru/arhlib/app/MainActivityTest.java
+++ app/src/androidTest/java/ru/arhlib/app/MainActivityTest.java
@@ -1,12 +1,12 @@
 package ru.arhlib.app;
 
-import androidx.test.espresso.contrib.RecyclerViewActions;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
 import static androidx.test.InstrumentationRegistry.getInstrumentation;
 import static androidx.test.espresso.Espresso.onView;
@@ -24,7 +24,7 @@ public class MainActivityTest {
 
     @Test
     public void navigateThroughApplication() {
-        onView(withText(R.string.afisha)).perform(click());
+        onView(withText(R.string.actual)).perform(click());
         onView(withText(R.string.services)).perform(click());
         onView(withText(R.string.contacts)).perform(click());
         pressBack();
@@ -32,16 +32,12 @@ public class MainActivityTest {
         onView(withText(R.string.about_licenses)).perform(click());
         pressBack();
         pressBack();
+        onView(withText(R.string.afisha)).perform(click());
+        pressBack();
         onView(withText(R.string.news)).perform(click());
-    }
-
-    @Test
-    public void openPostInBrowser() {
         onView(withId(R.id.news_list))
                 .perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
-
         openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-
         onView(withText(R.string.post_open_in_browser)).perform(click());
     }
 }
