a8e291d8622475bfd373d87fa87a99b97e4c60c8
ilimurzin <ilimurzin@ya.ru>
2019-05-06 00:40:28 +0300

    Пилим интерфейс в разделе Актуальное
    Добавлен жест для обновления
    Добавлен индикатор загрузки
    Добавлен вывод ошибки

--- app/src/main/java/ru/arhlib/app/actual/ActualFragment.java
+++ app/src/main/java/ru/arhlib/app/actual/ActualFragment.java
@@ -10,7 +10,11 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.Snackbar;
+
 import ru.arhlib.app.R;
+import ru.arhlib.app.databinding.ActualFragmentBinding;
 
 public class ActualFragment extends Fragment {
 
@@ -20,15 +24,22 @@ public class ActualFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.actual_fragment, container, false);
+        ActualFragmentBinding binding = ActualFragmentBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
-        if (view instanceof RecyclerView) {
-            ActualViewModel viewModel = ViewModelProviders.of(this).get(ActualViewModel.class);
-            RecyclerView recyclerView = (RecyclerView) view;
-            ActualAdapter adapter = new ActualAdapter();
-            viewModel.getActual().observe(this, adapter::submitList);
-            recyclerView.setAdapter(adapter);
-        }
+        ActualViewModel viewModel = ViewModelProviders.of(this).get(ActualViewModel.class);
+        RecyclerView recyclerView = view.findViewById(R.id.actual_list);
+        ActualAdapter adapter = new ActualAdapter();
+        viewModel.actual.observe(this, adapter::submitList);
+        viewModel.showErrorSnackbar.observe(this, (needToShow) -> {
+            if (needToShow) {
+                Snackbar.make(view, R.string.loading_error, Snackbar.LENGTH_SHORT).show();
+                viewModel.showErrorSnackbar.postValue(false);
+            }
+        });
+        recyclerView.setAdapter(adapter);
+        binding.setViewModel(viewModel);
+
         return view;
     }
 }
--- app/src/main/java/ru/arhlib/app/actual/ActualViewModel.java
+++ app/src/main/java/ru/arhlib/app/actual/ActualViewModel.java
@@ -1,10 +1,13 @@
 package ru.arhlib.app.actual;
 
-import java.util.List;
+import android.view.View;
 
-import androidx.lifecycle.LiveData;
+import androidx.databinding.ObservableBoolean;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -13,30 +16,66 @@ import ru.arhlib.app.data.Webservice;
 
 public class ActualViewModel extends ViewModel {
 
-    private MutableLiveData<List<Actual>> actual;
+    public MutableLiveData<List<Actual>> actual;
+    public ObservableBoolean showError;
+    public ObservableBoolean showProgressBar;
+    public ObservableBoolean showPullToRefresh;
+    public MutableLiveData<Boolean> showErrorSnackbar;
     private Webservice webservice;
 
     public ActualViewModel() {
+        actual = new MutableLiveData<>();
+        showError = new ObservableBoolean();
+        showProgressBar = new ObservableBoolean();
+        showPullToRefresh = new ObservableBoolean();
+        showErrorSnackbar = new MutableLiveData<>();
         webservice = Api.getInstance();
+
+        showProgressBar.set(true);
+        fetch();
     }
 
-    public LiveData<List<Actual>> getActual() {
-        if (actual == null) {
-            actual = new MutableLiveData<>();
-            webservice.getActual().enqueue(new Callback<List<Actual>>() {
-                @Override
-                public void onResponse(Call<List<Actual>> call, Response<List<Actual>> response) {
-                    if (response.isSuccessful()) {
-                        actual.postValue(response.body());
-                    }
-                }
+    public void onSwipeRefresh() {
+        showPullToRefresh.set(true);
+        fetch();
+    }
 
-                @Override
-                public void onFailure(Call<List<Actual>> call, Throwable t) {
-                    call.clone().enqueue(this);
+    public void onButtonClick(View view) {
+        showProgressBar.set(true);
+        fetch();
+    }
+
+    private void fetch() {
+        showError.set(false);
+        webservice.getActual().enqueue(new Callback<List<Actual>>() {
+            @Override
+            public void onResponse(Call<List<Actual>> call, Response<List<Actual>> response) {
+                stopLoading();
+                if (response.isSuccessful()) {
+                    actual.postValue(response.body());
+                } else {
+                    showError();
                 }
-            });
+            }
+
+            @Override
+            public void onFailure(Call<List<Actual>> call, Throwable t) {
+                stopLoading();
+                showError();
+            }
+        });
+    }
+
+    private void stopLoading() {
+        showProgressBar.set(false);
+        showPullToRefresh.set(false);
+    }
+
+    private void showError() {
+        if (actual.getValue() == null) {
+            showError.set(true);
+        } else {
+            showErrorSnackbar.postValue(true);
         }
-        return actual;
     }
 }
