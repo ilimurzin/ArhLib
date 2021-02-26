7f5d4c72ec992ee0deb4ec564506b894fd4c8768
ilimurzin <ilimurzin@ya.ru>
2021-02-26 17:49:26 +0300

    Перешёл на ViewPager2

--- app/src/main/java/ru/arhlib/app/MainActivity.kt
+++ app/src/main/java/ru/arhlib/app/MainActivity.kt
@@ -3,29 +3,36 @@ package ru.arhlib.app
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import androidx.viewpager.widget.ViewPager
-import com.google.android.material.tabs.TabLayout
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import ru.arhlib.app.actual.ActualFragment
+import ru.arhlib.app.databinding.MainActivityBinding
 import ru.arhlib.app.menu.MenuFragment
 import ru.arhlib.app.news.PostFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        val binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val viewPager = findViewById<ViewPager>(R.id.pager)
-        val tabLayout = findViewById<TabLayout>(R.id.tabs)
+        val viewPager = binding.pager
+        val tabLayout = binding.tabs
 
-        viewPager.adapter = SectionsPagerAdapter(supportFragmentManager)
-        viewPager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
-        tabLayout.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(viewPager))
+        viewPager.adapter = SectionsPagerAdapter(this)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Новости"
+                1 -> "Актуальноe"
+                else -> "Сервисы"
+            }
+        }.attach()
     }
 
-    private class SectionsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-        override fun getItem(position: Int): Fragment {
+    private class SectionsPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+        override fun createFragment(position: Int): Fragment {
             return when (position) {
                 0 -> PostFragment()
                 1 -> ActualFragment()
@@ -33,6 +40,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        override fun getCount(): Int = 3
+        override fun getItemCount(): Int = 3
     }
 }
